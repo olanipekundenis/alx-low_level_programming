7ef1ddfc713715e4664886ceb659f3146c7bbad6
denis <denisstillwaters@gmail.com>
2023-03-21 09:13:26 -0700

    ammending for betty

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,30 +1,25 @@
 #include "main.h"
 
 /**
- * print_sign - Determines if the input number
- * greater, equal or less than zero.
- *
- * @n: The input number as an integer.
- *
- * Return: is greater than zero. 0 is zero.
- * -1 is less than zero.
+ * print_sign - Prints the sign of a number
+ * @n: The number to be checked
+ * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
-		return (1);
-	} 
+	_putchar(43);
+	return (1);
+	}
 	else if (n < 0)
 	{
-		_putchar(45);
-		return (-1);
-	} 
+	_putchar(45);
+	return (-1);
+	}	
 	else
 	{
-		_putchar(48);
-		return (0);
-	}	
-	_putchar('\n');
+	_putchar(48);
+	return (0);
+	}
 }
